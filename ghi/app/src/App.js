4a5dc81253f0bff2826ec7c93afbbdcf584fb9b4
christsoi <christstststs@gmail.com>
2022-09-13 11:57:58 -0700

    Added ManufacturersList component

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-import AppointmentHistory from './AppointmentHistory';
+import ManufacturersList from './inventory/ManufacturersList';
 
 
 function App() {
@@ -11,8 +11,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} /> 
+          <Route path="manufacturers">
+            <Route index element={<ManufacturersList />} />  
+        </Route>
         </Routes>
-
       </div>
     </BrowserRouter>
   );
