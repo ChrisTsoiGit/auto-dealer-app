af768cf5554c3b4ca71ed4e0f3e85dd39509d2f9
christsoi <christstststs@gmail.com>
2022-09-15 12:52:13 -0700

    Finshed adding functionality

--- ghi/app/src/Service/AppointmentHistory.js
+++ ghi/app/src/Service/AppointmentHistory.js
@@ -37,7 +37,7 @@ class AppointmentHistory extends React.Component {
                         <input value={this.state.key} onChange={this.handleKeyChange} 
                         type="text" className="form-control" 
                         placeholder="Search by VIN" id="key" name="key" />
-                            <span className="input-group-text"><b>Search by VIN</b></span>
+                            <button className="input-group-text"><b>Search by VIN</b></button>
                     </div>
                 </form>   
                 <h2 className="mt-5"><b>Service Appointments History</b></h2>
@@ -62,7 +62,12 @@ class AppointmentHistory extends React.Component {
                             return (
                                 <tr key={ appointment.id } className={ignored}>
                                     <td>{ appointment.vin }</td>
-                                    <td>{ appointment.vip }</td>
+                                    <td>{ appointment.vip ? 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                        </svg> : " "}
+                                    </td>
                                     <td>{ appointment.owner }</td>
                                     <td>{ new Date(appointment.date).toLocaleDateString() }</td>
                                     <td>{ new Date(appointment.date).toLocaleTimeString() }</td>
